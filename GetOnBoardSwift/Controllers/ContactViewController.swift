27fa6ac99fb164d72ba50088b2c9a93121fef906
Rokkrs <rokkrs@MacBook-Pro-de-Oscar.local>
2017-01-05 16:44:36 -0300

    Create A scrollView Contact function

--- GetOnBoardSwift/Controllers/ContactViewController.swift
+++ GetOnBoardSwift/Controllers/ContactViewController.swift
@@ -10,26 +10,19 @@ import UIKit
 
 class ContactViewController: UIViewController {
 
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        // Para ser reconocido por el ViewController nuestra scrollView se debe agregar
+        view.addSubview(scrollView)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSize(width: 375, height: 800)
     }
-    */
 
 }
